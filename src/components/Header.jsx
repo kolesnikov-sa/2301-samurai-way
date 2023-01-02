2f53448771c447358bfe2f9cd1cd0431917bc6c5
Sergey Kolesnikov <kolesnikov-sa@ya.ru>
2023-01-02 17:31:52 +0300

    230102 вечер: первый коммит — урок 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return(
+		<header className="header">
+			<img src="https://www.svgrepo.com/show/340598/logo-glassdoor.svg" alt="Logo"></img>
+		</header>
+	);
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+	return (
+		<div className="content">
+			<div>
+				<img className="content__header-image" src="https://luatkhoa.org/wp-content/uploads/2014/10/blue-header-foocamp21.png" alt="Hero" />
+			</div>
+			<div>
+				Ava + description
+			</div>
+			<div>
+				My posts
+				<div>New post</div>
+				<div>post 1</div>
+				<div>post 2</div>
+			</div>
+		</div>
+	);
+}
+
+export default Profile;
